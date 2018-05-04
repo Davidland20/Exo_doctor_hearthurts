2341917fc39fa3ae60392953e2127574c64caaae
Davidland20 <david_bodou@yahoo.fr>
2018-05-04 09:31:25 +0200

    Document manquant

---
+++ index.js
@@ -0,0 +1,6 @@
+function writeText(txt) {
+    document.getElementById("Epaule")élément.innerHTML = Articulation qui unit le bras au thorax. (P.N.A. articulatis humeri.)
+
+Par extension, le terme épaule recouvre la région du corps correspondant à cette articulation.
+
+L'épaule est formée par l'articulation scapulohumérale, unissant l'humérus et l'omoplate, et les masses musculaires qui l'entourent. Elle comprend la région deltoïdienne, la région axillaire, ou aisselle, et la région scapulaire. L'articulation est composée par la tête de l'humérus, ronde, qui se loge dans la cavité correspondante de l'omoplate, la glène, ou cavité glénoïde. Les surfaces articulaires sont maintenues en place par une capsule enveloppante épaisse, par des ligaments puissants et par les tendons des muscles qui s'insèrent sur l'humérus. C'est une articulation très mobile qui permet des mouvements du bras de grande amplitude et en tous sens.;
